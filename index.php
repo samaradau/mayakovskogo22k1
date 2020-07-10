64fd6b7cd77e0946304840f917d30f94c144d8e6
Юрий Саморядов <samorad1998@yandex.ru>
2020-07-11 00:51:05 +0300

    Added project structure

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include_once 'header.php'; ?>
+
+<h1>Главная</h1>
+<p class="mt-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores eligendi sapiente temporibus quod officia esse dolor architecto soluta eveniet facilis quos perspiciatis, cumque animi delectus, perferendis dolorum ex minima sed.</p>
+
+<?php include_once 'footer.php'; ?>
